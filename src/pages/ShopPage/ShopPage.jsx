6a2ac233a9630644d797612c005d1b63d1b3f8ed
its-mohammad-js <mohammadreactjs@gmail.com>
2023-12-31 17:32:56 +0330

    deploy test: shop page => hero section

--- src/pages/ShopPage/ShopPage.jsx
+++ src/pages/ShopPage/ShopPage.jsx
@@ -70,41 +70,48 @@ function ShopHeroSection() {
 
   return (
     <div className="container mx-auto 2xl:max-w-6xl">
-      <div id="wrapper" className="h-screen py-20 px-2 flex w-full gap-x-2">
-        <div className="w-1/2 flex flex-col items-center justify-center gap-y-1">
-          {/* title slider */}
-          <Slider {...settings} className="bg-Buff-100 w-full h-1/3 rounded-md">
+      <div id="wrapper" className="h-screen py-20 px-2 w-full">
+        {/* slide gallery */}
+        <div className="flex gap-x-2">
+          {/* slide text */}
+          <div className="w-1/2 flex flex-col items-center justify-center gap-y-1">
+            {/* title slider */}
+            <Slider
+              {...settings}
+              className="bg-Buff-100 w-full h-1/3 rounded-md"
+            >
+              {shopHeroSectionInfo.map((slide, index) => (
+                <div
+                  key={index}
+                  className="text-center text-sm rounded-t-md py-8"
+                >
+                  <span>{slide.title}</span>
+                </div>
+              ))}
+            </Slider>
+            {/* slide description */}
+            <div className="bg-EerieBlack-600 text-white-100 text-xs px-3 py-2 w-full h-1/3 rounded-md overflow-auto">
+              {shopHeroSectionInfo[selectedSlide].desc}
+            </div>
+          </div>
+          {/* slide images */}
+          <div className="w-1/2 flex flex-col gap-y-1 max-h-full">
             {shopHeroSectionInfo.map((slide, index) => (
               <div
                 key={index}
-                className="text-center text-sm h-28 rounded-t-md py-8"
+                onClick={() => changeSlideHandler(index)}
+                className={`${
+                  selectedSlide === index ? "flex-1" : "h-10 flex-none"
+                }  rounded-md transition-all duration-1000`}
               >
-                <span>{slide.title}</span>
+                <img
+                  src={slide.bgUrl}
+                  alt="slide-bg"
+                  className={`h-full w-full object-cover rounded-md`}
+                />
               </div>
             ))}
-          </Slider>
-          {/* slide description */}
-          <div className="bg-EerieBlack-600 text-white-100 text-xs px-2 py-1 w-full h-1/3 rounded-md overflow-auto">
-            {shopHeroSectionInfo[selectedSlide].desc}
           </div>
-        </div>
-        {/* slide images */}
-        <div className="w-1/2 flex flex-col gap-y-1 max-h-full">
-          {shopHeroSectionInfo.map((slide, index) => (
-            <div
-              key={index}
-              onClick={() => changeSlideHandler(index)}
-              className={`${
-                selectedSlide === index ? "flex-1" : "h-10 flex-none"
-              }  rounded-md transition-all duration-1000`}
-            >
-              <img
-                src={slide.bgUrl}
-                alt="slide-bg"
-                className={`h-full w-full object-cover rounded-md`}
-              />
-            </div>
-          ))}
         </div>
       </div>
     </div>
